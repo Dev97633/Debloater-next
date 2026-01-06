47a3e6dd60072edd1fb8e73bd1a73c3520621c98
DEV <devendar87502@gmail.com>
2026-01-06 18:08:01 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
     id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
+repositories {
+    google()
+    mavenCentral()
+}
