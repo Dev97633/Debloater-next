201bfbc540627a3a4a7c5c60a14b45b443ea0b6e
DEV <devendar87502@gmail.com>
2026-01-07 11:37:33 +0530

    Update MainActivity.kt

--- app/src/main/kotlin/com/example/debloater/MainActivity.kt
+++ app/src/main/kotlin/com/example/debloater/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
