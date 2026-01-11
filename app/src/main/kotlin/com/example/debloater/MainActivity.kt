bea883e889ad8d0435f4ee3fa8bc0372ef8b0777
DEV <devendar87502@gmail.com>
2026-01-11 12:59:42 +0530

    Update MainActivity.kt

--- app/src/main/kotlin/com/example/debloater/MainActivity.kt
+++ app/src/main/kotlin/com/example/debloater/MainActivity.kt
@@ -43,6 +43,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //ShizukuManager.init(this)
+        val prefs = getSharedPreferences("intro_prefs", MODE_PRIVATE)
+if (prefs.getBoolean("intro_done", false)) {
+    ShizukuManager.init(applicationContext)
+}
         setContent {
             DebloaterTheme {
                 val snackbarHostState = remember { SnackbarHostState() }
