319ec7f209b1cb87f7b8a53d6f614274cb1fd592
DEV <devendar87502@gmail.com>
2026-04-09 14:33:27 +0530

    Update AppListViewModel.kt

--- app/src/main/kotlin/com/dev/debloater/apps/AppListViewModel.kt
+++ app/src/main/kotlin/com/dev/debloater/apps/AppListViewModel.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import java.util.Locale
